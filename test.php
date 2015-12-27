e65cb128e69ee3ef036742d8185f96b70d26a383
John Doe <johndoe@example.com>
2015-12-27 13:26:36 -0500

    Removed text from test.php

--- test.php
+++ test.php
@@ -1,20 +1,3 @@
 <?
-class Math{
-	public function kvadrat($a){
-	return $a*$a;
-	}
-	public function minus($a){
-	$b = 10-$a;
-	 return $b;
-	}
-	
-	public function cos($a){
-	if ($a == '1/2'){
-	echo "60";
-	}
-	}
-}
-
-$test = new Math();
-echo $test->kvadrat(16.56);
+//any text here
 ?>
